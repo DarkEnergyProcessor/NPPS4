a3e034768d654d488b1d704fa9f2ffb324bda561
Miku AuahDark <lumi.hotaru@gmail.com>
2023-05-02 17:40:51 +0800

    Add HTTP code 500 handler.

--- npps4/app.py
+++ npps4/app.py
@@ -1,3 +1,5 @@
+import traceback
+
 import fastapi
 import fastapi.responses
 
@@ -11,4 +13,12 @@ def handler_404(request: fastapi.Request, exc: Exception):
     return fastapi.responses.JSONResponse(
         status_code=404,
         content={"detail": f"Endpoint not found: {request.url}"},
+    )
+
+
+@core.exception_handler(500)
+def handler_500(request: fastapi.Request, exc: Exception):
+    return fastapi.responses.JSONResponse(
+        status_code=500,
+        content={"exception": type(exc).__name__, "message": str(exc), "traceback": traceback.format_exception(exc)},
     )
