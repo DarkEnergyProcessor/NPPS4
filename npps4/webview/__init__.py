49758a14d0e2a054bb2b475a5354e5bb02e62f8e
Miku AuahDark <lumi.hotaru@gmail.com>
2023-05-09 13:34:44 +0800

    Maintenance page.

--- npps4/webview/__init__.py
+++ npps4/webview/__init__.py
@@ -1,1 +1,11 @@
+import fastapi.responses
+
+from .. import app
 from . import tos
+
+
+@app.core.get("/resources/maintenace/maintenance.php", response_class=fastapi.responses.HTMLResponse)
+@app.core.get("/resources/maintenance/maintenance.php", response_class=fastapi.responses.HTMLResponse)
+def maintenance_page():
+    with open("templates/maintenance.html", "rb") as f:
+        return f.read()
