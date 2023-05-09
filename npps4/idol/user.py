cb573abbbeb483a651038337284bce525bf6eb72
Miku AuahDark <lumi.hotaru@gmail.com>
2023-05-09 21:29:38 +0800

    Fix main db access

--- npps4/idol/user.py
+++ npps4/idol/user.py
@@ -1,5 +1,5 @@
-from .. import db
 from .. import idol
+from ..db import main
 from ..idol import system
 
 from typing import overload
@@ -8,11 +8,11 @@ from typing import overload
 def get(context: idol.SchoolIdolParams, id: int | None = None):
     if isinstance(context, idol.SchoolIdolUserParams):
         id = context.token.user_id
-    return context.db.main.get(db.main.User, id)
+    return context.db.main.get(main.User, id)
 
 
 def create(context: idol.SchoolIdolParams, key: str, passwd: str):
-    user = db.main.User(key=key)
+    user = main.User(key=key)
     user.set_passwd(passwd)
     context.db.main.add(user)
     context.db.main.flush()
@@ -22,5 +22,5 @@ def create(context: idol.SchoolIdolParams, key: str, passwd: str):
 
 
 def find_by_key(context: idol.SchoolIdolParams, key: str):
-    user = context.db.main.query(db.main.User).where(db.main.User.key == key).first()
+    user = context.db.main.query(main.User).where(main.User.key == key).first()
     return user
