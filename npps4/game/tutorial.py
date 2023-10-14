e1b67a4606840bd036549c6c0342492e16c19921
Miku AuahDark <lumi.hotaru@gmail.com>
2023-10-14 20:33:32 +0800

    Implement first phase of tutorial.

--- npps4/game/tutorial.py
+++ npps4/game/tutorial.py
@@ -1,5 +1,7 @@
 from .. import idol
 from .. import util
+from ..idol import error
+from ..idol.system import user
 
 import pydantic
 
@@ -9,6 +11,18 @@ class TutorialProgressRequest(pydantic.BaseModel):
 
 
 @idol.register("/tutorial/progress", batchable=False)
-async def tutorial_progress(context: idol.SchoolIdolUserParams, request: TutorialProgressRequest) -> pydantic.BaseModel:
-    util.log("STUB /tutorial/progress", request, severity=util.logging.WARNING)
+async def tutorial_progress(
+    context: idol.SchoolIdolUserParams, request: TutorialProgressRequest
+) -> pydantic.BaseModel:
+    current_user = await user.get_current(context)
+    if current_user.tutorial_state == -1:
+        raise error.IdolError(detail="Tutorial already finished")
+    if current_user.tutorial_state == 0 and request.tutorial_state == 1:
+        current_user.tutorial_state = 1
+
+    util.log(
+        f"STUB /tutorial/progress, user {current_user.tutorial_state} request {request.tutorial_state}",
+        request,
+        severity=util.logging.WARNING,
+    )
     return pydantic.BaseModel()
