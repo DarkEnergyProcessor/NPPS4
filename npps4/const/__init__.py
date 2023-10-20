e34354e3128a3769205e16d40ae0b4786fb5f711
Miku AuahDark <lumi.hotaru@gmail.com>
2023-10-20 23:44:23 +0800

    Add ADdType to consts.

---
+++ npps4/const/__init__.py
@@ -0,0 +1,1 @@
+from . import ADD_TYPE
--- npps4/idol/system/other.py
+++ npps4/idol/system/other.py
@@ -1,5 +1,6 @@
 from . import item
 from . import unit
+from ... import const
 from ... import idol
 from ... import util
 from ...idol.system import achievement
@@ -9,7 +10,7 @@ from ...db import main
 
 async def add_item(context: idol.BasicSchoolIdolContext, user: main.User, item: item.Item):
     match item.add_type:
-        case 1000:
+        case const.ADD_TYPE.ITEM:
             match item.item_id:
                 case 2:
                     user.social_point = user.social_point + item.amount
@@ -19,17 +20,17 @@ async def add_item(context: idol.BasicSchoolIdolContext, user: main.User, item: 
                     user.free_sns_coin = user.free_sns_coin + item.amount
                 case _:
                     pass  # TODO
-        case 1001:
+        case const.ADD_TYPE.UNIT:
             unit_cnt = await unit.count_units(context, user, True)
             if unit_cnt < user.unit_max:
                 await unit.add_unit(context, user, item.item_id, True)
             else:
                 raise ValueError("max unit reached")  # TODO: Proper idol.error.IdolError
-        case 3000:
+        case const.ADD_TYPE.GAME_COIN:
             user.game_coin = user.game_coin + item.amount
-        case 3001:
+        case const.ADD_TYPE.LOVECA:
             user.free_sns_coin = user.free_sns_coin + item.amount
-        case 3002:
+        case const.ADD_TYPE.SOCIAL_POINT:
             user.social_point = user.social_point + item.amount
         case _:
             pass
