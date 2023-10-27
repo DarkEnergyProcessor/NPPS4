464f9b06d2f508b509b4f448d23cb00dbeb3151d
Miku AuahDark <lumi.hotaru@gmail.com>
2023-10-27 16:20:31 +0800

    Stub /scenario/startup to get story reading working.

--- npps4/game/scenario.py
+++ npps4/game/scenario.py
@@ -15,6 +15,15 @@ class ScenarioStatusResponse(pydantic.BaseModel):
     scenario_status_list: list[ScenarioStatus]
 
 
+class ScenarioStartupRequest(pydantic.BaseModel):
+    scenario_id: int
+
+
+class ScenarioStartupResponse(pydantic.BaseModel):
+    scenario_id: int
+    scenario_adjustment: int = 50  # TODO where to get this value
+
+
 @idol.register("/scenario/scenarioStatus")
 async def scenario_scenariostatus(context: idol.SchoolIdolUserParams) -> ScenarioStatusResponse:
     # TODO
@@ -24,3 +33,12 @@ async def scenario_scenariostatus(context: idol.SchoolIdolUserParams) -> Scenari
             ScenarioStatus(scenario_id=i, status=2) for i in itertools.chain(range(1, 4), range(184, 189))
         ]
     )
+
+
+@idol.register("/scenario/startup")
+async def scenario_startup(
+    context: idol.SchoolIdolUserParams, request: ScenarioStartupRequest
+) -> ScenarioStartupResponse:
+    # TODO
+    util.log("STUB /scenario/startup", severity=util.logging.WARNING)
+    return ScenarioStartupResponse(scenario_id=request.scenario_id)
