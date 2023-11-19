369537a925781febd9e5afdd152283c5c29708e4
Miku AuahDark <lumi.hotaru@gmail.com>
2023-11-19 14:08:04 +0800

    Implement /background/set.

--- npps4/game/background.py
+++ npps4/game/background.py
@@ -1,5 +1,6 @@
 from .. import idol
 from .. import util
+from ..idol import error
 from ..idol.system import background
 from ..idol.system import user
 
@@ -16,6 +17,10 @@ class BackgroundInfoResponse(pydantic.BaseModel):
     background_info: list[BackgroundInfo]
 
 
+class BackgroundSetRequest(pydantic.BaseModel):
+    background_id: int
+
+
 @idol.register("/background/backgroundInfo")
 async def background_backgroundinfo(context: idol.SchoolIdolUserParams) -> BackgroundInfoResponse:
     current_user = await user.get_current(context)
@@ -30,3 +35,13 @@ async def background_backgroundinfo(context: idol.SchoolIdolUserParams) -> Backg
     ]
 
     return BackgroundInfoResponse(background_info=background_info)
+
+
+@idol.register("/background/set")
+async def background_set(context: idol.SchoolIdolUserParams, request: BackgroundSetRequest) -> idol.core.DummyModel:
+    current_user = await user.get_current(context)
+    if await background.has_background(context, current_user, request.background_id):
+        current_user.active_background = request.background_id
+        return idol.core.DummyModel()
+
+    raise error.IdolError(detail="No such background")
