7408ebdfead58137dc45c4688a38e8fce0281d3e
Miku AuahDark <lumi.hotaru@gmail.com>
2023-12-23 16:17:59 +0800

    Improve script type annotation.

--- npps4/script.py
+++ npps4/script.py
@@ -6,6 +6,11 @@ from . import config
 from typing import Protocol
 
 
+class Script(Protocol):
+    async def run_script(self, args: list[str]):
+        ...
+
+
 def load_script(path: str):
     return config.load_module_from_file(path, "npps4_script_run")
 
@@ -15,7 +20,8 @@ async def main():
     parser.add_argument("script", type=load_script, help="Script to run.")
 
     args, unk_args = parser.parse_known_args()
-    await args.script.run_script(unk_args)
+    script: Script = args.script
+    await script.run_script(unk_args)
 
 
 if __name__ == "__main__":
