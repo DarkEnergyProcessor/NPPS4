b0864b1d8c792670c09177986c1b837b534bc635
HanaKochia <162745842+hanakochia@users.noreply.github.com>
2024-03-09 15:08:34 -0500

    Update npps4/webui/endpoints/list_users.py
    Co-authored-by: Miku AuahDark <auahdark687291@gmail.com>

--- npps4/webui/endpoints/list_users.py
+++ npps4/webui/endpoints/list_users.py
@@ -9,14 +9,6 @@ import sqlalchemy
 from ... import idol
 from ...db import main
 
-async def run_script(args: list[str]):
-    context = npps4.idol.BasicSchoolIdolContext(lang=npps4.idol.Language.en)
-    async with context:
-        q = sqlalchemy.select(npps4.db.main.User)
-        result = await context.db.main.execute(q)
-        users = [ {'name':user.name, 'id':user.id} for user in result.scalars()]
-
-    return users
 
 @webui.app.get("/list_users.html")
 async def list_users(request: fastapi.Request):
