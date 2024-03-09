28f2bd677e56f8000e2246113517a9123e09dc44
HanaKochia <hanakochia@gmail.com>
2024-03-09 15:50:58 -0500

    Implemented suggested fixes:
      * Cleaned up imports in list_users.py and unlock_backgrounds.py
      * moved list_users helped function into the main function and made it
        async
      * ran "black -l 120" over unlock_backgrounds.py

--- npps4/webui/endpoints/list_users.py
+++ npps4/webui/endpoints/list_users.py
@@ -1,22 +1,18 @@
 import fastapi
-import asyncio
-
-from .. import template
-from ...app import webui
-
 import sqlalchemy
 
 from ... import idol
 from ...db import main
+from .. import template
+from ...app import webui
 
 
 @webui.app.get("/list_users.html")
 async def list_users(request: fastapi.Request):
-    async with idol.BasicSchoolIdolContext(lang=idol.Language.en):
+    async with idol.BasicSchoolIdolContext(lang=idol.Language.en) as context:
         q = sqlalchemy.select(main.User)
         result = await context.db.main.execute(q)
         users = [ {'name':user.name, 'id':user.id} for user in result.scalars()]
 
     return template.template.TemplateResponse("list_users.html", {"request": request, "users": users})
 
-    return template.template.TemplateResponse("list_users.html", {"request": request, "users": users})
