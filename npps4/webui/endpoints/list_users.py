cda886db9cd629b1caf11edf37ffea4354e08057
Miku AuahDark <lumi.hotaru@gmail.com>
2024-03-11 11:29:56 +0800

    Reorder imports and reformat list_users and unlock_backgrounds WebUI.

--- npps4/webui/endpoints/list_users.py
+++ npps4/webui/endpoints/list_users.py
@@ -1,10 +1,10 @@
 import fastapi
 import sqlalchemy
 
-from ... import idol
-from ...db import main
 from .. import template
+from ... import idol
 from ...app import webui
+from ...db import main
 
 
 @webui.app.get("/list_users.html")
@@ -12,7 +12,6 @@ async def list_users(request: fastapi.Request):
     async with idol.BasicSchoolIdolContext(lang=idol.Language.en) as context:
         q = sqlalchemy.select(main.User)
         result = await context.db.main.execute(q)
-        users = [ {'name':user.name, 'id':user.id} for user in result.scalars()]
+        users = [{"name": user.name, "id": user.id} for user in result.scalars()]
 
     return template.template.TemplateResponse("list_users.html", {"request": request, "users": users})
-
--- npps4/webui/endpoints/unlock_backgrounds.py
+++ npps4/webui/endpoints/unlock_backgrounds.py
@@ -3,12 +3,12 @@ import urllib.parse
 import fastapi
 import sqlalchemy
 
-from ...idol.system import background
+from .. import template
 from ... import idol
+from ...app import webui
 from ...db import item
 from ...db import main
-from .. import template
-from ...app import webui
+from ...idol.system import background
 
 
 async def get_backgrounds(uid: str, to_unlock: list[str]) -> tuple[list[item.Background], list[item.Background]]:
@@ -30,7 +30,9 @@ async def get_backgrounds(uid: str, to_unlock: list[str]) -> tuple[list[item.Bac
                 unlocked_ids.append(bg_id)
 
         all_backgrounds = result.scalars().all()
-        unlocked_backgrounds: list[item.Background] = [bg for bg in all_backgrounds if bg.background_id in unlocked_ids]
+        unlocked_backgrounds: list[item.Background] = [
+            bg for bg in all_backgrounds if bg.background_id in unlocked_ids
+        ]
         locked_backgrounds: list[item.Background] = [
             bg for bg in all_backgrounds if bg.background_id not in unlocked_ids
         ]
