d347d79410a9c76a3393aa11ce2680edc90300c1
Miku AuahDark <lumi.hotaru@gmail.com>
2024-03-11 12:28:27 +0800

    Rewrite unlock_backgrounds.py.

--- npps4/webui/endpoints/unlock_backgrounds.py
+++ npps4/webui/endpoints/unlock_backgrounds.py
@@ -10,44 +10,41 @@ from ...db import item
 from ...db import main
 from ...idol.system import background
 
+from typing import Annotated
 
-async def get_backgrounds(uid: str, to_unlock: list[str]) -> tuple[list[item.Background], list[item.Background]]:
-    async with idol.BasicSchoolIdolContext(lang=idol.Language.en) as context:
-        target_user = await context.db.main.get(main.User, uid)
 
-        if target_user is None:
-            return [], []
+async def get_backgrounds(context: idol.BasicSchoolIdolContext, user: main.User):
+    q = sqlalchemy.select(item.Background)
+    result = await context.db.item.execute(q)
+    all_backgrounds = {k.background_id: k for k in result.scalars()}
 
-        q = sqlalchemy.select(item.Background)
-        result = await context.db.item.execute(q)
+    user_backgrounds = await background.get_backgrounds(context, user)
+    unlocked_ids = set(bg.id for bg in user_backgrounds)
+    locked_ids = set(all_backgrounds.keys()) - unlocked_ids
 
-        user_backgrounds = await background.get_backgrounds(context, target_user)
-        unlocked_ids = [bg.id for bg in user_backgrounds]
-
-        for bg_id in map(int, to_unlock):
-            if bg_id not in unlocked_ids:
-                await background.unlock_background(context, target_user, bg_id)
-                unlocked_ids.append(bg_id)
-
-        all_backgrounds = result.scalars().all()
-        unlocked_backgrounds: list[item.Background] = [
-            bg for bg in all_backgrounds if bg.background_id in unlocked_ids
-        ]
-        locked_backgrounds: list[item.Background] = [
-            bg for bg in all_backgrounds if bg.background_id not in unlocked_ids
-        ]
+    unlocked_backgrounds = [all_backgrounds[i] for i in sorted(unlocked_ids)]
+    locked_backgrounds = [all_backgrounds[i] for i in sorted(locked_ids)]
 
     return unlocked_backgrounds, locked_backgrounds
 
 
 @webui.app.get("/unlock_backgrounds.html")
-async def unlock_backgrounds(request: fastapi.Request):
-    uid: str = request.query_params["uid"]
-    gs: dict[str, list[str]] = urllib.parse.parse_qs(str(request.query_params))
-    to_unlock: list[str] = "bg_id" in gs and gs["bg_id"] or []
-    unlocked, locked = await get_backgrounds(uid, to_unlock)
+async def unlock_backgrounds(
+    request: fastapi.Request, uid: int, bg_id: Annotated[list[int], fastapi.Query(default_factory=list)]
+):
+    async with idol.BasicSchoolIdolContext(lang=idol.Language.en) as context:
+        target_user = await context.db.main.get(main.User, uid)
+        if target_user is None:
+            raise ValueError("invalid user_id")
 
-    return template.template.TemplateResponse(
-        "unlock_backgrounds.html",
-        {"uid": uid, "request": request, "unlocked_backgrounds": unlocked, "locked_backgrounds": locked},
-    )
+        # Perform unlock first.
+        for background_id in bg_id:
+            await background.unlock_background(context, target_user, background_id)
+
+        # Get locked and unlocked backgrounds
+        unlocked, locked = await get_backgrounds(context, target_user)
+
+        return template.template.TemplateResponse(
+            "unlock_backgrounds.html",
+            {"uid": uid, "request": request, "unlocked_backgrounds": unlocked, "locked_backgrounds": locked},
+        )
