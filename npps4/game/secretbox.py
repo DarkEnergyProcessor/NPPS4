2181d503fe23c62cb911083aebf03a989b1a8e78
Miku AuahDark <lumi.hotaru@gmail.com>
2024-03-27 15:40:41 +0800

    Scouting: Add const values related to secretbox data model.

--- npps4/game/secretbox.py
+++ npps4/game/secretbox.py
@@ -1,3 +1,6 @@
+# import pickle
+
+from .. import const
 from .. import idol
 from .. import util
 from ..system import common
@@ -11,7 +14,7 @@ class SecretboxGaugeInfo(pydantic.BaseModel):
 
 
 class SecretboxAllAnimationAsset(pydantic.BaseModel):
-    type: int
+    type: const.SECRETBOX_ANIMATION_TYPE
     background_asset: str
     additional_asset_1: str
     additional_asset_2: str
@@ -22,20 +25,39 @@ class SecretboxAllCost(pydantic.BaseModel):
     id: int
     payable: int
     unit_count: int
-    type: int  # add_type
-    item_id: int
+    type: const.SECRETBOX_COST_TYPE
+    # Note: IF "type" above is "LOVECA", then possible item_id values:
+    # * 1: Paid loveca
+    # * anything else: Free loveca + paid loveca
+    item_id: int | None = None
     amount: int
 
 
 class SecretboxAllButton(pydantic.BaseModel):
-    secret_box_button_type: int
+    secret_box_button_type: const.SECRETBOX_BUTTON_TYPE
     cost_list: list[SecretboxAllCost]
     secret_box_name: str
 
 
+class SecretboxAllButtonShowCost(pydantic.BaseModel):
+    cost_type: const.SECRETBOX_COST_TYPE
+    item_id: int | None = None
+    unit_count: int
+
+
+class SecretboxAllButtonWithShowCost(SecretboxAllButton):
+    show_cost: SecretboxAllButtonShowCost
+
+
+AnySecretboxButton = SecretboxAllButton | SecretboxAllButtonWithShowCost
+
+
 class SecretboxAllSecretboxInfo(pydantic.BaseModel):
     secret_box_id: int
-    secret_box_type: int
+    # Some button naming difference:
+    # DEFAULT: Uses "name" field in this class when pressing button.
+    # STUB: Uses "secret_box_name" field in the button list.
+    secret_box_type: const.SECRETBOX_LAYOUT_TYPE
     name: str
     description: str | None = None
     start_date: str
@@ -46,17 +68,24 @@ class SecretboxAllSecretboxInfo(pydantic.BaseModel):
     pon_upper_limit: int = 0
 
 
+class SecretboxAllSecretboxInfoWithShowEndDate(SecretboxAllSecretboxInfo):
+    show_end_date: str
+
+
+AnySecretboxInfo = SecretboxAllSecretboxInfo | SecretboxAllSecretboxInfoWithShowEndDate
+
+
 class SecretboxAllPage(pydantic.BaseModel):
     menu_asset: str
     page_order: int
     animation_assets: SecretboxAllAnimationAsset
-    button_list: list[SecretboxAllButton]
-    secret_box_info: SecretboxAllSecretboxInfo
+    button_list: list[AnySecretboxButton]
+    secret_box_info: AnySecretboxInfo
 
 
 class SecretboxAllMemberCategory(pydantic.BaseModel):
     member_category: int
-    page_list: list
+    page_list: list[SecretboxAllPage]
 
 
 class SecretboxAllResponse(pydantic.BaseModel):
@@ -72,3 +101,6 @@ async def secretbox_all(context: idol.SchoolIdolUserParams) -> SecretboxAllRespo
     # TODO
     util.stub("secretbox", "all")
     raise idol.error.by_code(idol.error.ERROR_CODE_LIB_ERROR)
+    # en = "" if context.is_lang_jp() else "en/"
+    # with open("secretbox.pickle", "rb") as f:
+    #     return pickle.load(f)
