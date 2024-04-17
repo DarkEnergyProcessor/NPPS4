a47c4ea487b4a83f3e3db93a62ad421c89c42b24
Miku AuahDark <lumi.hotaru@gmail.com>
2024-04-17 16:52:02 +0800

    Script: Add script to geenrate passcode for particular account.

---
+++ scripts/generate_passcode.py
@@ -0,0 +1,28 @@
+import argparse
+
+import npps4.idol
+import npps4.system.handover
+import npps4.scriptutils.user
+
+
+async def run_script(arg: list[str]):
+    parser = argparse.ArgumentParser(__file__)
+    group = parser.add_mutually_exclusive_group(required=True)
+    npps4.scriptutils.user.register_args(group)
+    parser.add_argument("passcode", default=None)
+    args = parser.parse_args(arg)
+
+    transfer_code = args.passcode or npps4.system.handover.generate_transfer_code()
+
+    async with npps4.idol.BasicSchoolIdolContext(lang=npps4.idol.Language.en) as context:
+        target_user = await npps4.scriptutils.user.from_args(context, args)
+        target_user.transfer_sha1 = npps4.system.handover.generate_passcode_sha1(target_user.invite_code, transfer_code)
+
+        print("Transfer ID:", target_user.invite_code)
+        print("Transfer Passcode:", transfer_code)
+
+
+if __name__ == "__main__":
+    import npps4.scriptutils.boot
+
+    npps4.scriptutils.boot.start(run_script)
