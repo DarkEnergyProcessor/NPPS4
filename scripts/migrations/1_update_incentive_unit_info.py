4c5992cbadcbe35923b7fdfd1e806db6649430f9
Miku AuahDark <lumi.hotaru@gmail.com>
2024-05-10 09:41:04 +0800

    Data Migration: Fix script.

--- scripts/migrations/1_update_incentive_unit_info.py
+++ scripts/migrations/1_update_incentive_unit_info.py
@@ -1,3 +1,4 @@
+import json
 import npps4.script_dummy  # Must be first
 
 import sqlalchemy
@@ -18,15 +19,17 @@ async def run_script(args: list[str]):
         with await context.db.main.execute(q) as result:
             for row in result.scalars():
                 hitit = False
-                if row.item_id in unit_cache_attribute:
-                    row.unit_rarity, row.unit_attribute = unit_cache_attribute[row.item_id]
-                    hitit = True
-                else:
+                if row.item_id not in unit_cache_attribute:
                     unit_info = await npps4.system.unit.get_unit_info(context, row.item_id)
                     if unit_info is not None:
                         unit_cache_attribute[row.item_id] = (unit_info.rarity, unit_info.attribute_id)
-                        row.unit_rarity, row.unit_attribute = unit_info.rarity, unit_info.attribute_id
-                        hitit = True
+
+                row.unit_rarity, row.unit_attribute = unit_cache_attribute[row.item_id]
+                if row.extra_data is not None:
+                    extra_data = json.loads(row.extra_data)
+                    if "is_support_member" in extra_data and (not extra_data["is_support_member"]):
+                        extra_data["attribute"] = row.unit_attribute
+                        row.extra_data = json.dumps(extra_data)
 
                 if hitit:
                     await context.db.main.flush()
