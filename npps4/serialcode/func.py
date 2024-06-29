b0f9c9937e8a9c492baf308bb7fe202b25db88e8
Miku AuahDark <lumi.hotaru@gmail.com>
2024-06-29 22:41:24 +0800

    Make previous serial code commit in present box instead.

--- npps4/serialcode/func.py
+++ npps4/serialcode/func.py
@@ -1,7 +1,11 @@
 import sqlalchemy
 
+from .. import const
 from .. import idol
 from ..db import main
+from ..system import advanced
+from ..system import item_model
+from ..system import reward
 from ..system import unit
 
 
@@ -11,6 +15,11 @@ async def give_all_supporter_units(context: idol.BasicSchoolIdolContext, user: m
     )
     result = await context.db.unit.execute(q)
     for unit_id in result.scalars():
-        await unit.add_supporter_unit(context, user, unit_id, 100)
+        item_data = await advanced.deserialize_item_data(
+            context, item_model.BaseItem(add_type=const.ADD_TYPE.UNIT, item_id=unit_id, amount=100)
+        )
+        await reward.add_item(
+            context, user, item_data, " 追いかける, ショー・ヘーレーション!", "Oikakeru, Snow Halation!"
+        )
 
     return "Given all supporter members (100x quantity each)."
