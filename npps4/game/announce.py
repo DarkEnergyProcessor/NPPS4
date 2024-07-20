4a8d948c34db09d8c410425b2cf4ca0c1cb9b00f
Miku AuahDark <lumi.hotaru@gmail.com>
2024-07-20 19:03:48 +0800

    Unstub announce/checkState.

--- npps4/game/announce.py
+++ npps4/game/announce.py
@@ -1,14 +1,18 @@
 from .. import idol
-from .. import util
 from ..system import common
+from ..system import reward
+from ..system import user
 
 
 class AnnounceStateResponse(common.TimestampMixin):
+    present_cnt: int
     has_unread_announce: bool
 
 
 @idol.register("announce", "checkState")
 async def announce_checkstate(context: idol.SchoolIdolUserParams) -> AnnounceStateResponse:
-    # TODO
-    util.stub("announce", "checkState", context.raw_request_data)
-    return AnnounceStateResponse(has_unread_announce=False)
+    current_user = await user.get_current(context)
+    return AnnounceStateResponse(
+        present_cnt=await reward.count_presentbox(context, current_user),
+        has_unread_announce=False,  # TODO
+    )
