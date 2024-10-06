5b5f84ffb3dfaa78d00318cea3f54376a544b969
Miku AuahDark <lumi.hotaru@gmail.com>
2024-10-06 09:27:03 +0800

    Live show: Consume LP.

--- npps4/game/live.py
+++ npps4/game/live.py
@@ -348,7 +348,7 @@ async def live_schedule(context: idol.SchoolIdolUserParams) -> LiveScheduleRespo
 
 
 DEBUG_SERVER_SCORE_CALCULATE = False
-DEBUG_SERVER_CONSUME_LP = False
+DEBUG_SERVER_CONSUME_LP = True
 
 
 @idol.register("live", "partyList")
