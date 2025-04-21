cc80b3ac56ccefa278beb4a39c9b358d76c9d771
Miku AuahDark <lumi.hotaru@gmail.com>
2025-04-21 22:45:16 +0800

    Fix converter script. Empty list is empty dict.

--- npps4/game/item.py
+++ npps4/game/item.py
@@ -5,12 +5,35 @@ from ..system import common
 from ..system import item
 from ..system import user
 
+from typing import Annotated, Any
+
+
+def empty_list_is_empty_dict(value: Any):
+    if isinstance(value, list) and len(value) == 0:
+        return {}
+    return value
+
+
+class ItemListReinforceInfoUnitReinforceItem(pydantic.BaseModel):
+    unit_reinforce_item_id: int
+    reinforce_type: int
+    addition_value: int
+    target_unit_ids: list[int]
+
+
+class ItemListReinforceInfo(pydantic.BaseModel):
+    event_id: int
+    item_list: list[ItemListReinforceInfoUnitReinforceItem]
+    available_unit_list: list[dict]  # Additionally has mark_id (int) and sub_evaluation (int)
+
 
 class ItemListResponse(pydantic.BaseModel):
     general_item_list: list[common.ItemCount]
     buff_item_list: list[common.ItemCount]
     reinforce_item_list: list[common.ItemCount]
-    reinforce_info: list = pydantic.Field(default_factory=list)
+    reinforce_info: Annotated[dict[str, ItemListReinforceInfo], pydantic.BeforeValidator(empty_list_is_empty_dict)] = (
+        pydantic.Field(default_factory=dict)
+    )
 
 
 @idol.register("item", "list")
