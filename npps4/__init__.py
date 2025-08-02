f3ca673be538364239f8360af9a47904eba02746
Miku AuahDark <lumi.hotaru@gmail.com>
2025-08-02 16:04:41 +0800

    Make sure psycopg works in Windows.

--- npps4/__init__.py
+++ npps4/__init__.py
@@ -17,3 +17,9 @@
 # 2.  Altered source versions must be plainly marked as such, and must not be
 #     misrepresented as being the original software.
 # 3.  This notice may not be removed or altered from any source distribution.
+
+import asyncio
+import os
+
+if os.name == "nt":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
