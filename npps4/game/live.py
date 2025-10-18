f9df0962fd20a2ad3638154eebd4d96eea951fce
Miku AuahDark <lumi.hotaru@gmail.com>
2025-10-18 17:07:57 +0800

    Always enable LP consumption.
    LP consumption is quite stable for now.

--- npps4/game/live.py
+++ npps4/game/live.py
@@ -360,25 +360,29 @@ async def live_schedule(context: idol.SchoolIdolUserParams) -> LiveScheduleRespo
 
 
 DEBUG_SERVER_SCORE_CALCULATE = False
-DEBUG_SERVER_CONSUME_LP = True
 
 
 @idol.register("live", "partyList")
 async def live_partylist(context: idol.SchoolIdolUserParams, request: LivePartyListRequest) -> LivePartyListResponse:
     current_user = await user.get_current(context)
 
-    # Check LP
+    # Check live info
     live_info = await live.get_live_info_table(context, request.live_difficulty_id)
     if live_info is None:
         raise idol.error.by_code(idol.error.ERROR_CODE_LIVE_NOT_FOUND)
-    if DEBUG_SERVER_CONSUME_LP:
-        if live_info.capital_type == 2:
+
+    # Check LP
+    cap_value = live_info.capital_value * request.lp_factor
+    match live_info.capital_type:
+        case 1:
+            if not user.has_energy(current_user, cap_value):
+                if current_user.energy_max < cap_value:
+                    raise idol.error.by_code(idol.error.ERROR_CODE_LIVE_NOT_ENOUGH_MAX_ENERGY)
+                else:
+                    raise idol.error.by_code(idol.error.ERROR_CODE_LIVE_NOT_ENOUGH_CURRENT_ENERGY)
+        case _:
             # TODO
             raise idol.error.by_code(idol.error.ERROR_CODE_LIVE_NOT_ENOUGH_EVENT_POINT)
-        if live_info.capital_type == 1 and not user.has_energy(
-            current_user, live_info.capital_value * request.lp_factor
-        ):
-            raise idol.error.by_code(idol.error.ERROR_CODE_LIVE_NOT_ENOUGH_CURRENT_ENERGY)
 
     live_setting = await live.get_live_setting(context, live_info.live_setting_id)
     if live_setting is None:
@@ -488,15 +492,22 @@ async def live_play(context: idol.SchoolIdolUserParams, request: LivePlayRequest
     live_info = await live.get_live_info_table(context, request.live_difficulty_id)
     if live_info is None:
         raise idol.error.by_code(idol.error.ERROR_CODE_LIVE_NOT_FOUND)
-    if DEBUG_SERVER_CONSUME_LP:
-        cap_value = live_info.capital_value * request.lp_factor
-        if live_info.capital_type == 2:
+
+    # Check LP
+    cap_value = live_info.capital_value * request.lp_factor
+    match live_info.capital_type:
+        case 1:
+            if not user.has_energy(current_user, cap_value):
+                if current_user.energy_max < cap_value:
+                    raise idol.error.by_code(idol.error.ERROR_CODE_LIVE_NOT_ENOUGH_MAX_ENERGY)
+                else:
+                    raise idol.error.by_code(idol.error.ERROR_CODE_LIVE_NOT_ENOUGH_CURRENT_ENERGY)
+        case _:
             # TODO
             raise idol.error.by_code(idol.error.ERROR_CODE_LIVE_NOT_ENOUGH_EVENT_POINT)
-        if live_info.capital_type == 1 and not user.has_energy(current_user, cap_value):
-            raise idol.error.by_code(idol.error.ERROR_CODE_LIVE_NOT_ENOUGH_CURRENT_ENERGY)
-        # Consume LP
-        user.sub_energy(current_user, cap_value)
+
+    # Consume LP
+    user.sub_energy(current_user, cap_value)
 
     live_setting = await live.get_live_setting_from_difficulty_id(context, request.live_difficulty_id)
     if live_setting is None:
